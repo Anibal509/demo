ca65f23c03bdec6cb07c58846612ea6fbb68c93e
anibal509 <dominguez_alveo20@hotmail.com>
2022-12-07 02:07:34 +0000

    ultimos cambios

--- src/main/java/com/prueba1/App.java
+++ src/main/java/com/prueba1/App.java
@@ -1,10 +1,86 @@
 package com.prueba1;
 
 import static spark.Spark.*;
+import org.json.JSONObject;
 
-public class App {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Cars objCars = new Cars();
+        
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request
+                    .headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers",
+                        accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request
+                    .headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods",
+                        accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        get("/cars", (req, res) -> {
+            res.type("application/json");
+            JSONObject jsonobj = new JSONObject(objCars);
+            return jsonobj;
+        });
+
+        get("/cars/:id", (req, res) -> {
+            res.type("application/json");
+            JSONObject jsonobj = new JSONObject(objCars.getCars().get(Integer.parseInt(req.params(":id")) - 1));
+            return jsonobj;
+        });
+
+        post("/cars", (req, res) -> {
+            res.type("application/json");
+            Car car = new Car();
+            JSONObject body = new JSONObject(req.body());
+            car.setId(objCars.getCars().size() + 1);
+            car.setName(body.getString("name"));
+            car.setYear(body.getInt("year"));
+            objCars.addCar(car);
+            JSONObject jsonobj = new JSONObject(objCars);
+            return jsonobj;
+        });
     }
 }
+
+
+//hecho x mi 
+
+/*public class App {
+    public static void main(String[] args) {
+       
+        Cars objCars = new Cars();
+        get("/hello", (req, res) -> {
+            res.type("application/json");
+            JSONObject jsonobj = new JSONObject(objCars); 
+            return jsonobj ;
+            });
+        post("/hello", (req, res) -> {
+            res.type("application/json");
+            Car car =new Car();
+            JSONObject body =new JSONObject(req.body());
+            car.setName(body.getString("name"));
+            car.setYear(body.getInt("Year"));
+            objCars.addCar(car);
+            JSONObject jsonobj=new JSONObject(objCars);
+            return jsonobj;
+            });
+    }
+}
+*/
+
+
+
 
---
+++ src/main/java/com/prueba1/Cars.java
@@ -0,0 +1,16 @@
+package com.prueba1;
+
+import java.util.ArrayList;
+
+public class Cars {
+    ArrayList<Car> cars= new ArrayList<Car>();
+
+public ArrayList<Car> getCars(){
+    return cars;
+}
+public void addCar(Car car) {
+    cars.add(car);
+}   
+
+}
+
